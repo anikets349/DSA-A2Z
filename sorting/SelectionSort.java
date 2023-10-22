91875752c9dd88ee23ef7452495033034b05ae4c
Aniket Saha <anikets349@gmail.com>
2023-10-22 20:53:57 +0530

    Learn Selection Sort

---
+++ sorting/SelectionSort.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void swap(int[] arr, int idx1, int idx2) {
+        int temp = arr[idx1];
+        arr[idx1] = arr[idx2];
+        arr[idx2] = temp;
+    }
+
+    // O(n^2) time, O(1) space
+    public static void selectionSort(int[] arr, int n) {
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            swap(arr, i, min);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        selectionSort(arr, n);
+        System.out.println(Arrays.toString(arr));
+        sc.close();
+    }
+}
