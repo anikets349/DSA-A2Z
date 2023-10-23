251f6f7ef26c5cceea2b454084a1558255d30f76
Aniket Saha <anikets349@gmail.com>
2023-10-23 16:33:06 +0530

    Add recursive implementation insertionSortRecurisve of Insertion Sort in InsertionSort.java

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -8,6 +8,15 @@ public class InsertionSort {
         arr[idx2] = temp;
     }
 
+    public static void insertionSortRecursive(int[] arr, int n, int i) {
+        if (i == n)
+            return;
+        for (int j = i; j > 0 && arr[j - 1] > arr[j]; j--) {
+            swap(arr, j - 1, j);
+        }
+        insertionSortRecursive(arr, n, i + 1);
+    }
+
     // O(n^2) in worst, avg, O(n) time in best, O(1) space
     public static void insertionSort(int[] arr, int n) {
         for (int i = 0; i < n; i++) {
@@ -25,6 +34,7 @@ public class InsertionSort {
             arr[i] = sc.nextInt();
         }
         insertionSort(arr, n);
+        // insertionSortRecursive(arr, n, 0);
         System.out.println(Arrays.toString(arr));
         sc.close();
     }
