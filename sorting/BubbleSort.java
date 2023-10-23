96a9a253bf892293bab21071e94f6fc60c4c3629
Aniket Saha <anikets349@gmail.com>
2023-10-23 16:40:22 +0530

    Add recursive implementation bubbleSortRecurisve of Bubble Sort in BubbleSort.java

--- sorting/BubbleSort.java
+++ sorting/BubbleSort.java
@@ -8,6 +8,17 @@ public class BubbleSort {
         arr[idx2] = temp;
     }
 
+    public static void bubbleSortRecursive(int[] arr, int n) {
+        if (n == 1)
+            return;
+        for (int j = 0; j < n - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr, j, j + 1);
+            }
+        }
+        bubbleSortRecursive(arr, n - 1);
+    }
+
     // O(n^2) time in worst, O(n) time in best, O(1) space
     public static void bubbleSort(int[] arr, int n) {
         for (int i = 0; i < n - 1; i++) {
@@ -30,7 +41,7 @@ public class BubbleSort {
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        bubbleSort(arr, n);
+        bubbleSortRecursive(arr, n);
         System.out.println(Arrays.toString(arr));
         sc.close();
     }
